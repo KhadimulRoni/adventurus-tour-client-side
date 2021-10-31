375b2dceb288badc7a74f37bff3d391a603b3a8b
KhadimulRoni <mdkhadimul7066@gmail.com>
2021-10-31 23:35:42 +0600

    login page edited

--- src/components/Home/Login/Login.js
+++ src/components/Home/Login/Login.js
@@ -4,7 +4,7 @@ import useAuth from '../../../Hooks/useAuth';
 const Login = () => {
     const {signInWithGoogle} = useAuth();
     return (
-        <div style={{height:"100vh"}}>
+        <div style={{height:"80vh"}}>
             <h2 className="tours p-4">- Please Login -</h2>
             <button onClick={signInWithGoogle} className="btn btn-warning">Google Sign In</button>
         </div>
