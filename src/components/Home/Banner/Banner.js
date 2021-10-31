be5a75e82c3aae6b14bc031b61752e095b693254
KhadimulRoni <mdkhadimul7066@gmail.com>
2021-10-31 22:49:38 +0600

    title resized

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Carousel } from 'react-bootstrap';
-import banner1 from "../../../images/banner/banner.1.jpg"
-import banner2 from "../../../images/banner/banner2.jpg"
-import banner3 from "../../../images/banner/banner3.jpg"
-
-import "./Banner.css"
+import banner1 from "../../../images/banner/banner.1.jpg";
+import banner2 from "../../../images/banner/banner2.jpg";
+import banner3 from "../../../images/banner/banner3.jpg";
+import "./Banner.css";
 
 const Banner = () => {
     return (
@@ -16,8 +15,8 @@ const Banner = () => {
                     src={banner1}
                     alt="First slide"
                     />
-                    <Carousel.Caption className="title">
-                        Change the world by being yourself
+                    <Carousel.Caption>
+                        <h1 className="title">Change the world by being yourself</h1>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -27,8 +26,8 @@ const Banner = () => {
                     alt="Second slide"
                     />
 
-                    <Carousel.Caption className="title">
-                        Every moment is a fresh beginning
+                    <Carousel.Caption>
+                        <h1 className="title">Every moment is a fresh beginning</h1>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
@@ -38,8 +37,8 @@ const Banner = () => {
                     alt="Third slide"
                     />
 
-                    <Carousel.Caption className="title">
-                        Die with memories, not dreams
+                    <Carousel.Caption>
+                        <h1 className="title">Die with memories, not dreams</h1>
                     </Carousel.Caption>
                 </Carousel.Item>
             </Carousel>
